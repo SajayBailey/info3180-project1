f1541c6f5b307e25fe83f6630a6b2382e04fa201
Sajay Bailey <sajay.bailey@mymona.uwi.edu>
2017-03-22 20:57:56 +0000

    project commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "wellth1s$ho^lb@ super #4m1$$3d key101l"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://test:pass@localhost/proj"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://comp3180:comp@localhost/project1"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
--- app/views.py
+++ app/views.py
@@ -4,19 +4,17 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-import os
-from app import app
-from flask import render_template, request, redirect, url_for
-import time
+
 from werkzeug.utils import secure_filename
-from forms import LoginForm
-from models import UserProfile
+from app import app, db
+from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
+# from forms import LoginForm
+from app.models import UserProfile
+import time
+import os, random
 
 
-def timeinfo():
-    return time.strftime("%a, %d %b %Y")
-    
 
 ###
 # Routing for your application.
@@ -27,51 +25,68 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
-    
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="INFO3180 PROJECT 1")
+    return render_template('about.html', name="Mary Jane")
 
-## route for adding a profile
-@app.route("/profile", methods=['POST', 'GET'])
-def profile_form():
+@app.route('/secure-page/')
+@login_required
+def secure_page():
+    """Render a secure page on our website that only logged in users can access."""
+    return render_template('secure_page.html')
     
+    
+# function to calculate time profile was created
+def timeinfo():
+    return time.strftime("%a, %d %b %Y")
+    
+    
+#route for adding a profile
+@app.route("/profile", method = ['GET', 'POST'])
+def add_profile():
+    
+    id_strt = random.randint(0,99999)
     prifile_pic_folder = 'app/static/pics'
-    form = LoginForm()
-    if request.method == "POST":
-        if form.validate_on_submit():
-            firstname = request.form['firstname']
-            lastname = request.form['lastname']
-            age = request.form['age']
-            sex = request.form['Sex']
-            bio = request.form['bio']
-            created_on = timeinfo()
-            profile_pic = request.form['prfile_pic']
-            
-            
-            # save pic to pics file
-            filename = secure_filename(profile_pic.filename)
-            profile_pic.save(os.path.join(prifile_pic_folder, filename))
-            
-            flash('Success!!!')
-            return render_template('home.html')
     
-    return render_template("profile_form.html")
-    
-   
-   
-## route for viewing a list of all user profiles
-# @app.route("/profiles", method = ["GET"])
-# def view_all_profiles():
-#     return render_template("all_profiles.html")
-    
+    if request.method == 'POST':
+        userid = id_strt
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        age = request.form['age']
+        sex = request.form['sex']
+        bio = request.form['bio']
+        created_on = timeinfo()
+        profile_pic = request.form['profile_pic']
+        username = request.form['username']
+        
+        
+        # save pic to pics file
+        filename = secure_filename(profile_pic.filename)
+        profile_pic.save(os.path.join(prifile_pic_folder, filename))
+        
+        user = UserProfile(userid, firstname,lastname,username, age, bio, created_on, profile_pic)
+        db.session.add(user)
+        db.session.commit()
+        flash('Success!!!')
+        
+            
+    return render_template('profile.html')
 
-## route for viewing an individual profile
-# @app.route("/profile/<userid>", method = ["GET"])
-# def search_profile():
-#     return
-    
+
+
+
+
+# Flash errors from the form if validation fails
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), 'danger')
+
+
 ###
 # The functions below should be applicable to all Flask apps.
 ###
@@ -90,7 +105,7 @@ def add_header(response):
     and also to cache the rendered page for 10 minutes.
     """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
+    response.headers['Cache-Control'] = 'public, max-age=0'
     return response
 
 
@@ -98,7 +113,6 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
-
 
 
 if __name__ == '__main__':
